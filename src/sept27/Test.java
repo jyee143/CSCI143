5e480be7edb9e577a4c05dc7deac14c016303209
Jenna Yee <jennayee143@gmail.com>
2018-09-27 21:54:58 -0700

    account change to myAccount

--- src/sept27/Test.java
+++ src/sept27/Test.java
@@ -2,15 +2,15 @@ package sept27;
 
 public class Test {
 	public static void main (String[] args) {
-		Account account = new Account (1122, 2000);
-		account.setAnnualInterestRate(4.5);
+		Account myAccount = new Account (1122, 2000);
+		myAccount.setAnnualInterestRate(4.5);
 		
-		account.setId(1234);
+		myAccount.setId(1234);
 		
-		account.withdraw(2500);
-		account.deposit(3000);
-		System.out.println("Balance is " + account.getBalance());
-		System.out.println("ID is " + account.getId());
+		myAccount.withdraw(2500);
+		myAccount.deposit(3000);
+		System.out.println("Balance is " + myAccount.getBalance());
+		System.out.println("ID is " + myAccount.getId());
 	}
 }
 
